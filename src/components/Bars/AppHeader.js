a9e5cabff61c89d63e98e8e0e79214a9c14b8722
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-02-21 22:23:45 +0700

    make back button in headerbar

--- src/components/Bars/AppHeader.js
+++ src/components/Bars/AppHeader.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { AppBar, Box, Button, Grid, Toolbar } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-
+import { useHistory } from "react-router-dom";
 //image
 import logo from "../../Static/image/logo.png";
 
 function AppHeader() {
+  const history = useHistory();
   return (
     <div>
       <Box>
         <AppBar position="static" style={{ background: "#5F498C" }}>
           <Toolbar>
-            <Button>
+            <Button onClick={() => history.goBack()}>
               <ArrowBackIosIcon sx={{ fontSize: 40, color: "white" }} />
             </Button>
             <Grid sx={{ marginLeft: "auto", marginRight: 0 }}>
