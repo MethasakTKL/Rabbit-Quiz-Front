b30ab6bad4161f299f15d7077fe3b0d8abbc446f
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-02-22 23:18:39 +0700

    Members in Class (Mockup)

--- src/pages/ClassMember.js
+++ src/pages/ClassMember.js
@@ -1,7 +1,11 @@
-import { Button, Box, Paper } from "@mui/material";
+import { Button, Box, Paper, Grid } from "@mui/material";
 import React from "react";
 import "./ClassMember.css";
-import Card from '@mui/material/Card';
+import Card from "@mui/material/Card";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FaceIcon from "@mui/icons-material/Face";
+import IconButton from "@mui/material/IconButton";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 function ClassMember() {
   return (
@@ -21,48 +25,158 @@ function ClassMember() {
         }}
         style={{ background: "#F19528" }}
       >
-        <h3 className="center" style={{ paddingLeft: "30%",paddingTop:5}}>สมาชิก</h3>
+        <h3 className="center" style={{ paddingLeft: "30%", paddingTop: 5 }}>
+          สมาชิก
+        </h3>
+        {/* // Mockup data----------------------------------------------------------------------------------------------------------------------- */}
       </Paper>
       <Box
         sx={{
-            width: "80%",
-            height: 40,
-            borderRadius: 20,
-            marginLeft: "auto",
-            marginRight: "auto",
-            paddingTop: 2,
+          width: "80%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 2,
         }}
       >
-        <Card variant="contained" style={{ background: "#CACACE"}} sx={{borderRadius:4}}>
-          <p className="centertext">นายชนาวัฒน์ ทั้วสุภาพ</p>
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <AccountCircleIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">อาจารย์ อารีย์ มีสุข</p>
+          </Grid>
         </Card>
       </Box>
+      {/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */}
       <Box
         sx={{
-            width: "80%",
-            height: 40,
-            borderRadius: 20,
-            marginLeft: "auto",
-            marginRight: "auto",
-            paddingTop: 6,
+          width: "75%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 7,
         }}
       >
-        <Card variant="contained" style={{ background: "#CACACE"}} sx={{borderRadius:4}}>
-          <p className="centertext">นายเมธาศักดิ์ ทิพย์กองลาศ </p>
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <FaceIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">ชนาวัฒน์ ทั้วสุภาพ</p>
+          </Grid>
         </Card>
       </Box>
+      {/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */}
       <Box
         sx={{
-            width: "80%",
-            height: 40,
-            borderRadius: 20,
-            marginLeft: "auto",
-            marginRight: "auto",
-            paddingTop: 9,
+          width: "75%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 10,
         }}
       >
-        <Card variant="contained" style={{ background: "#CACACE"}} sx={{borderRadius:4}}>
-          <p className="centertext">นายชนาวัฒน์ ทั้วสุภาพ</p>
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <FaceIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">เมธาศักดิ์ ทิพย์กองลาศ</p>
+          </Grid>
+        </Card>
+      </Box>
+      {/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */}
+      <Box
+        sx={{
+          width: "75%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 10,
+        }}
+      >
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <FaceIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">นัฏฐวัฒน์ สิงห์อินทร์</p>
+          </Grid>
+        </Card>
+      </Box>
+      {/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */}
+      <Box
+        sx={{
+          width: "75%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 10,
+        }}
+      >
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <FaceIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">อิฟฟาฮาน สุขสุวรรณ</p>
+          </Grid>
+        </Card>
+      </Box>
+      {/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */}
+      <Box
+        sx={{
+          width: "75%",
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingTop: 10,
+        }}
+      >
+        <Card variant="contained" sx={{ borderRadius: 4 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingLeft={3}
+            sx={{ background: "#E4E4E5" }}
+          >
+            <FaceIcon sx={{ fontSize: 40 }} />
+            <p className="centertext">ธรรมาธิป ชิตพงศ์</p>
+          </Grid>
         </Card>
       </Box>
     </div>
