b95f0adc684c0d6ada4a291c82f88bca75e09406
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-02-23 20:52:52 +0700

    Recent Post (Mockup)

--- src/pages/ClassRoom.js
+++ src/pages/ClassRoom.js
@@ -5,10 +5,12 @@ import "./ClassRoom.css";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import QuizIcon from "@mui/icons-material/Quiz";
 import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FaceIcon from "@mui/icons-material/Face";
 
 function Classroom() {
   return (
-    <div>
+    <div className="screen">
       <h1 className="classname" style={{ paddingLeft: "5%", fontSize: 36 }}>
         ห้องเรียน 240-124
       </h1>
@@ -20,7 +22,7 @@ function Classroom() {
           to="/classroom-member"
           component={Link}
         >
-          <PeopleAltIcon /> 
+          <PeopleAltIcon />
           <div className="button1">สมาชิก</div>
         </Button>
         <Button
@@ -30,7 +32,7 @@ function Classroom() {
           to="/classroom-activity"
           component={Link}
         >
-          <QuizIcon /> 
+          <QuizIcon />
           <div className="button2">กิจกรรม</div>
         </Button>
       </Stack>
@@ -46,7 +48,7 @@ function Classroom() {
             borderRadius: 3,
             marginLeft: "auto",
             marginRight: "auto",
-            paddingBottom: 2
+            paddingBottom: 2,
           },
         }}
       >
@@ -54,25 +56,25 @@ function Classroom() {
           <h1 className="post" style={{ paddingLeft: 40, fontSize: 24 }}>
             ประกาศข้อความของคุณ
           </h1>
-          <Grid  direction="row" spacing={2} direction='row'>
-            <Grid item className="center" >
-            <TextField
-              label="สร้างโพสต์"
-              placeholder="พิมพ์ข้อความที่ต้องการบอกสมาชิกในห้องเรียน"
-              margin="normal"
-              style={{ width: "90%" }}
-              multiline
-            />
+          <Grid direction="row" spacing={2} direction="row">
+            <Grid item className="center">
+              <TextField
+                label="สร้างโพสต์"
+                placeholder="พิมพ์ข้อความที่ต้องการบอกสมาชิกในห้องเรียน"
+                margin="normal"
+                style={{ width: "90%" }}
+                multiline
+              />
             </Grid>
             <Grid className="postbutton">
-            <Button
-              className="postbutton"
-              variant="contained"
-              sx={{ width: '100%', height: 50, borderRadius: 4 }}
-              style={{ background: "#5F498C" }}
-            >
-              <div className="postbuttontext">โพสต์</div>
-            </Button>
+              <Button
+                className="postbutton"
+                variant="contained"
+                sx={{ width: "100%", height: 50, borderRadius: 4 }}
+                style={{ background: "#5F498C" }}
+              >
+                <div className="postbuttontext">โพสต์</div>
+              </Button>
             </Grid>
           </Grid>
         </Paper>
@@ -85,14 +87,70 @@ function Classroom() {
             m: 1,
 
             width: "90%",
-            height: 200,
+            height: "auto",
             borderRadius: 3,
             marginLeft: "auto",
             marginRight: "auto",
           },
         }}
       >
-        <Paper elevation={3}></Paper>
+        <Paper elevation={3}>
+          <h1 className="post" style={{ paddingLeft: 40, fontSize: 24 }}>
+            โพสต์ล่าสุด
+          </h1>
+          {/* --------------------------------------------------------------------------------------------------------------------------------------- */}
+          <Paper
+            style={{
+              background: "#E7E6E5",
+              width: "80%",
+              height: "auto",
+              margin: "auto",
+              borderRadius: 10,
+            }}
+          >
+            <Grid container direction="row" alignItems="center" paddingLeft={2}>
+              <AccountCircleIcon sx={{ fontSize: 30, color: "#9C2431" }} />
+              <p className="postername">อารีย์ มีสุข</p>
+            </Grid>
+            <p className="postinfo">
+              ให้นักเรียนทำกิจกรรมที่ 2 ให้เสร็จภายในวันที่ 3 มีนาคม 2565
+            </p>
+          </Paper>
+          {/* --------------------------------------------------------------------------------------------------------------------------------------- */}
+          <Paper
+            style={{
+              background: "#E7E6E5",
+              width: "80%",
+              height: "auto",
+              margin: "auto",
+              borderRadius: 10,
+            }}
+          >
+            <Grid container direction="row" alignItems="center" paddingLeft={2}>
+              <FaceIcon sx={{ fontSize: 30, color: "#F19528" }} />
+              <p className="postername">อิฟฟาฮาน สุขสุวรรณ</p>
+            </Grid>
+            <p className="postinfo">คุณครูครับ ต้องทำกิจกรรมที่ 2 เลยไหมครับ</p>
+          </Paper>
+          {/* --------------------------------------------------------------------------------------------------------------------------------------- */}
+          <Paper
+            style={{
+              background: "#E7E6E5",
+              width: "80%",
+              height: "auto",
+              margin: "auto",
+              borderRadius: 10,
+            }}
+          >
+            <Grid container direction="row" alignItems="center" paddingLeft={2}>
+              <AccountCircleIcon sx={{ fontSize: 30, color: "#9C2431" }} />
+              <p className="postername">อารีย์ มีสุข</p>
+            </Grid>
+            <p className="postinfo">
+              ให้นักเรียนทำกิจกรรมที่ 1 ให้เสร็จภายในวันที่ 24 กุมภาพันธ์ 2565
+            </p>
+          </Paper>
+        </Paper>
       </Box>
     </div>
   );
