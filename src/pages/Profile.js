5eb7f58bd2963ded014c98682c9bdf1d816379fd
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-02-23 23:56:36 +0700

    Add  Logout Button in Profile page Adjust Icon in button

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,9 @@ import React from "react";
 import FaceIcon from "@mui/icons-material/Face";
 import { Box } from "@mui/system";
 import "./Profile.css";
+import { Link } from "react-router-dom";
+import LogoutIcon from '@mui/icons-material/Logout';
+import SaveIcon from '@mui/icons-material/Save';
 
 function Profile() {
   return (
@@ -20,6 +23,12 @@ function Profile() {
             <div className="centerName">ชนาวัฒน์ ทั้วสุภาพ</div>
             <div className="centerAccount">ประเภทบัญชี : นักเรียน</div>
           </Grid>
+          <Grid textAlign='center' paddingBottom={2}>
+            <Button variant="contained" style={{background:'#CD0049'}} to='/login' component={Link}>
+              <div className="logoutIcon"><LogoutIcon sx={{fontSize:20}}/></div>
+              <div className="logout" >ออกจากระบบ</div>
+            </Button>
+          </Grid>
         </Paper>
       </Grid>
       <Grid paddingTop={2}>
@@ -34,28 +43,27 @@ function Profile() {
           <h1 className="editTitle" style={{ paddingLeft: 40, fontSize: 24 }}>
             แก้ไขข้อมูลส่วนตัว
           </h1>
-          <Grid width={'70%'} margin='auto'>
+          <Grid width={"70%"} margin="auto">
             <TextField
               fullWidth
               id="standard-basic"
               label="ชื่อ"
               defaultValue="ชนาวัฒน์"
- 
               variant="filled"
             />
           </Grid>
-          <Grid width={'70%'} margin='auto' paddingTop={2} paddingBottom={2}>
+          <Grid width={"70%"} margin="auto" paddingTop={2} paddingBottom={2}>
             <TextField
               fullWidth
               id="standard-basic"
               label="นามสกุล"
               defaultValue="ทั้วสุภาพ"
               variant="filled"
-
             />
           </Grid>
-          <Grid paddingBottom={2} sx={{marginLeft:'60%'}}>
-            <Button variant="contained" sx={{width:'60%'}}>
+          <Grid paddingBottom={2} sx={{ marginLeft: "60%" }}>
+            <Button variant="contained" sx={{ width: "60%" }}>
+              <div className="saveButtonIcon"><SaveIcon/></div>
               <div className="saveButton">บันทึก</div>
             </Button>
           </Grid>
