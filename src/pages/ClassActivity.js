63a82868e431640058e72d412dc5c19f73eee5c5
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-02-26 21:26:58 +0700

    Popup Activity when click (ClassActivity)

--- src/pages/ClassActivity.js
+++ src/pages/ClassActivity.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import { Button, Box, Paper } from "@mui/material";
-import './ClassActivity.css'
-function ClassActivity() {
+import React from "react";
+import { Button, Box, Paper, Grid, Fab, IconButton } from "@mui/material";
+import "./ClassActivity.css";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { height } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
+//import image
+import waterplant from "../Static/image/waterplant.png";
 
+function ClassActivity() {
+  const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div>
       <h1 className="classname" style={{ paddingLeft: "5%", fontSize: 36 }}>
@@ -20,7 +40,9 @@ function ClassActivity() {
         }}
         style={{ background: "#51D87A" }}
       >
-        <h3 className="center" style={{ paddingLeft: "30%",paddingTop:5}}>กิจกรรม</h3>
+        <h3 className="center" style={{ paddingLeft: "30%", paddingTop: 5 }}>
+          กิจกรรม
+        </h3>
       </Paper>
       <Box
         sx={{
@@ -30,7 +52,8 @@ function ClassActivity() {
             m: 1,
 
             width: "90%",
-            height: 'auto',
+            maxWidth: 1000,
+            height: "auto",
             borderRadius: 3,
             marginLeft: "auto",
             marginRight: "auto",
@@ -38,10 +61,54 @@ function ClassActivity() {
         }}
       >
         <Paper elevation={3}>
-          <div className='activityclass'>ยังไม่มีกิจกรรม</div>
+          <Grid paddingTop={2} paddingBottom={2} paddingLeft={"20%"}>
+            <Button
+              variant="outlined"
+              onClick={handleClickOpen}
+              sx={{ width: 500, height: 100 }}
+            >
+              <h1 className="activitybutton">
+                กิจกรรมที่ 1 รดน้ำต้นไม้วันที่ 1
+              </h1>
+            </Button>
+          </Grid>
+          <Dialog open={open} onClose={handleClose} fullWidth={fullWidth}>
+            <DialogTitle className="titledialog" >
+              <a className="activityNo">กิจกรรมที่ 1</a>
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText className="dialogtext">
+                ต้องการจะรดน้ำต้นไม้หรือไม่
+              </DialogContentText>
+              <img
+                src={waterplant}
+                style={{ width: 400, paddingLeft: "30%", paddingTop: 30 }}
+              />
+            </DialogContent>
+            <Grid paddingTop={2} paddingBottom={2}>
+              <DialogActions>
+                <Button
+                  variant="contained"
+                  onClick={handleClose}
+                  className="buttonno"
+                  style={{ width: 150, height: 50, background: "#D9534F" }}
+                >
+                  ไม่รดน้ำ
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={handleClose}
+                  className="buttonyes"
+                  style={{ width: 150, height: 50, background: "#5BC0DE" }}
+                >
+                  รดน้ำ
+                </Button>
+              </DialogActions>
+            </Grid>
+          </Dialog>
         </Paper>
       </Box>
     </div>
   );
 }
-export default ClassActivity
+export default ClassActivity;
