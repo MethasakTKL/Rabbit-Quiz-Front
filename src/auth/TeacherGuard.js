7f627b37963abef98e2e34ae7353699dcb67c96b
Chanawat Thuasuphap <64101110092@email.psu.ac.th>
2022-03-23 16:02:15 +0700

    If login as teacher direct path to /teacher

---
+++ src/auth/TeacherGuard.js
@@ -0,0 +1,33 @@
+import { useLocation, Outlet, Navigate } from "react-router";
+import { useAuth, appAuthProvider } from "./auth";
+import { useNavigate } from "react-router-dom";
+
+function RequireAuth() {
+    let auth = useAuth();
+    let location = useLocation();
+    let navigate = useNavigate
+
+    if (auth.user === null) {
+        let token = localStorage.getItem('access_token')
+        if (token) {
+            try {
+                let username = localStorage.getItem('id_username')
+                let password = localStorage.getItem('id_password')
+                let userdata = { username, password }
+
+                auth.signin(userdata, (response) => {
+                    auth.setUser(response)
+                })
+                return <Outlet />
+
+            } catch (err) {
+                console.log("Error", JSON.stringify(err))
+            }
+        }
+
+        return <Navigate to="/" state={{ from: location }} />;
+    }
+    return <Outlet />;
+}
+
+export { RequireAuth };
