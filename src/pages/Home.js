0e6338dd5d8805197e6dd89de7c049dc647ca9da
Chanawat Thuasuphap <64101110092@email.psu.ac.th>
2022-03-23 23:11:45 +0700

    Use React.Component & npm i react-navite

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,74 +22,47 @@ import CreateClassRoomPopup from "../components/Popup/CreateClassRoomPopup";
 import UserClassRoomCard from "../components/User/UserClassRoomCard";
 
 
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
 
+    render() {
+        let userFirstName = localStorage.getItem('user_first_name')
+        let userIsStaff = JSON.parse(localStorage.getItem('user_is_staff'))
+        return (
+            <div>
+                <h4 className="hello">สวัสดี, {userFirstName}</h4>
+                {/* ADD AND CREATE CLASSROOM SECTION USER*/}
+                {userIsStaff ? <CreateClassRoomPopup /> : <AddClassRoomPopup />}
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        "& > :not(style)": {
+                            m: 1,
+                            width: "90%",
+                            maxWidth: 1000,
+                            borderRadius: 3,
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                        },
+                    }}
+                >
+                    <Paper elevation={3}>
+                        <h1 className="titleclass" sx={{}}>
+                            ห้องเรียน <ClassIcon sx={{ fontSize: "50" }} />
+                        </h1>
+                        <UserClassRoomCard />
 
-
-function Home() {
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    let userFirstName = localStorage.getItem('user_first_name')
-    let userIsStaff = JSON.parse(localStorage.getItem('user_is_staff'))
-    const [userClassRoom, setUserClassRoom] = useState()
-
-    let timeout;
-    useEffect(() => {
-
-        async function fetchData() {
-            const classRoom = await ax.get('/getUserClassroom')
-
-            let roomDetail = classRoom.data; let n = 0; let rooms = []
-            for (const prop in roomDetail) {
-                let room_id = roomDetail[n].id
-                let room_name = roomDetail[n].name
-                rooms.push({ id: room_id, name: room_name })
-                n++
-            }
-            // console.log("obj room", rooms)
-        }
-        fetchData();
-        // <---- ใช้ useEffect async fucntion เพื่อลดการเรียกใช้ fetchData
-    }, []);
-
-
-
-    return (
-        <div>
-            <h4 className="hello">สวัสดี, {userFirstName}</h4>
-            {/* ADD AND CREATE CLASSROOM SECTION USER*/}
-            {userIsStaff ? <CreateClassRoomPopup /> : <AddClassRoomPopup />}
-            <Box
-                sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    "& > :not(style)": {
-                        m: 1,
-                        width: "90%",
-                        maxWidth: 1000,
-                        borderRadius: 3,
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                    },
-                }}
-            >
-                <Paper elevation={3}>
-                    <h1 className="titleclass" sx={{}}>
-                        ห้องเรียน <ClassIcon sx={{ fontSize: "50" }} />
-                    </h1>
-                    <UserClassRoomCard />
-
-                </Paper>
-            </Box>
-        </div>
-    );
+                    </Paper>
+                </Box>
+            </div >
+        );
+    }
 }
 
 export default Home;
