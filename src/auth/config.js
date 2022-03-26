c267ccfe31fa404b0e99d1a393e08646a565351e
Iffahan Suksuwan <jackcopsluncher@gmail.com>
2022-03-27 02:09:40 +0700

    localhost

--- src/auth/config.js
+++ src/auth/config.js
@@ -2,7 +2,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
     isProd,
-    UrlPrefix: isProd ? '/' : 'http://localhost:8000/'
+    UrlPrefix: isProd ? '/' : 'localhost:8000/'
 }
 
 export default config;
